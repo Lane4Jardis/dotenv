a70895bfc71400c6d2772cad0e47d41a387af1cd
Rolf Beab <54433986+Lane4Digital@users.noreply.github.com>
2025-01-02 09:10:06 +0100

    switch from simple git flow to more professionell flow (#1)
    * switch from simple git flow to more professionell flow
     - avoid commits to main
     - use branches
     - commit restrictions
     - branch name restrictions
    
    * added CODEOWNERS

--- support/pre-commit-hook.sh
+++ support/pre-commit-hook.sh
@@ -8,21 +8,21 @@ user="$(git config user.name)"
 gitDir="$(git rev-parse --git-dir)"
 files=$(git diff --cached --name-only --diff-filter=ACMR -- '*.php')
 
-#branch="$(git rev-parse --abbrev-ref HEAD)"
-#commits="$(git rev-list --count HEAD ^${branch})"
-#pattern="^(feature|fix|hotfix)\/[0-9]{6,7}_[a-zA-Z0-9_-]+|:[0-9a-f]{7,40}$"
+branch="$(git rev-parse --abbrev-ref HEAD)"
+commits="$(git rev-list --count HEAD ^${branch})"
+pattern="^(feature|fix|hotfix)\/[0-9]{6,7}_[a-zA-Z0-9_-]+|:[0-9a-f]{7,40}$"
 
 if [[ -d "${gitDir}/rebase-merge" || -d "${gitDir}/rebase-apply" ]]; then
     exit 0;
 fi
 
-#if [[ $commits -eq 0 ]]; then
-#    echo "Validate branch name..."
-#    if [[ ! $branch_name =~ $pattern ]]; then
-#        echo -e "\e[1;31mCommit not valid because of branch name '${branch}' !\n\e[0m"
-#        exit 1;
-#    fi
-#fi
+if [[ $commits -eq 0 ]]; then
+    echo "Validate branch name..."
+    if [[ ! $branch_name =~ $pattern ]]; then
+        echo -e "\e[1;31mCommit not valid because of branch name '${branch}' !\n\e[0m"
+        exit 1;
+    fi
+fi
 
 echo "Committing as user ${user}"
 if [[ $user =~ [.,:'!@#$%^&*()_+'] ]]; then
